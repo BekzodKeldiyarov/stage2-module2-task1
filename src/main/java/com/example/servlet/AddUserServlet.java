a75c83820a7d0510b5e2cdd0e2750ce5fb530e79
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2023-02-20 00:38:45 +0500

    Done

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,30 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        Warehouse.getInstance().addUser(user);
+        resp.sendRedirect("/add");
+//        getServletContext().getRequestDispatcher("/add").forward(req, resp);
+    }
 }
